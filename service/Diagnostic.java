abf34d176878ccc42da3956ffb8dd7779b7f8eef
Banea Sonia-Gabriela <46330499+niissaao@users.noreply.github.com>
2020-05-29 10:45:09 +0300

    Add files via upload

---
+++ service/Diagnostic.java
@@ -0,0 +1,30 @@
+package service;
+
+import model.Afectiune;
+import model.Pacient;
+
+public class Diagnostic
+{
+    public String puneDiagnostic (Pacient pacient, Afectiune[] afectiuni)
+    {
+        for(Afectiune a : afectiuni)
+        {
+            for(String sa : a.getSimptome())
+            {
+                int k = 0;
+                for (String s : pacient.getSimptome()) {
+                    if (s.equals(sa))
+                    {
+                        k++;
+                    }
+                }
+                if(k == 3)
+                {
+                    pacient.setDiagnostic(a.getDenumire());
+                }
+            }
+        }
+        return pacient.getDiagnostic();
+    }
+    
+}
